32d81a482e7081e7775a59d7a411ca2c6dd40d8c
mayifan <1316191756@qq.com>
2022-09-22 21:30:08 +0800

    second commit

--- src/main/java/com/mayifan/TestGit.java
+++ src/main/java/com/mayifan/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
 
     public static void main(String[] args) {
         System.out.println("mayifan test maven git--1");
+        System.out.println("mayifan test maven git--2");
     }
 }
